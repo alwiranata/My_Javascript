ec84ce59f2979136ec47d34ec63a0ffbe18ddaa9
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-12-23 15:51:42 +0700

    file : add function print

--- js/output.js
+++ js/output.js
@@ -2,10 +2,15 @@ const p = document.getElementById("value")
 const write = document.write(5+ 6)
 p.innerHTML = "Aldo Wiranta"
 
+
 function addWrite(){
     write
 }
 
 function showAlert(){
     alert (5 + 5)
+}
+
+function addPrint(){
+    const print1 = window.print()
 }
