b86b0794dabf66b1e01f904811d3880f788f6228
Aldo Wiranata <awiranata263@gmail.com>
2024-12-23 10:24:54 +0700

    add : file output js and html

---
+++ js/output.js
@@ -0,0 +1,11 @@
+const p = document.getElementById("value")
+const write = document.write(5+ 6)
+p.innerHTML = "Aldo Wiranta"
+
+function addWrite(){
+    write
+}
+
+function showAlert(){
+    alert (5 + 5)
+}
