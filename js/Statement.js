e2337f2221cb52f3cec1cda7be9f2159c3e7383c
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-12-24 07:55:39 +0700

    file : add statement js and html

---
+++ js/Statement.js
@@ -0,0 +1,15 @@
+let x ,y ,z 
+x = 10
+y = 6
+z = x + y
+console.log(z)
+
+const myId = document.getElementById("myId").innerHTML = "Aldo Wiranata"
+
+function clickButton(){
+    document.getElementById("click1").innerHTML = 
+    "Hello Aldo"
+    document.getElementById("click2").innerHTML =
+    "Hello Wiranata"
+}
+
