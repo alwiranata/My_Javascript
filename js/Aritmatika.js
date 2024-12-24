35410999ed025665361d673e1e6c5e2e41705b9a
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-12-24 14:38:19 +0700

    file : add Aritmatika.JS

---
+++ js/Aritmatika.js
@@ -0,0 +1,22 @@
+ //aritmatika
+
+ const p = 10
+ const a = 2
+ let z = p + a
+ console.log(z)
+ z = p - a
+ console.log(z)
+ z = p * a
+ console.log(z)
+ z = p /a 
+ console.log(z)
+ z = p % a
+ console.log(z)
+ z = p ** a
+ console.log(z)
+ z = Math.pow(p ,a)
+ console.log(z)
+ z = p + a * p
+ console.log(z)
+ z = (p + a)*p
+ console.log(z) 
