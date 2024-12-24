e5826b5144b4c678cc7d997b0926d167caeb01bb
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-12-24 13:57:07 +0700

    file : add Operators.js

---
+++ js/Operators.js
@@ -0,0 +1,41 @@
+//operator penuagasan =
+
+let x =10
+let y = 20
+
+//operator penjumlahan +
+
+let z = x + y
+console.log(z)
+
+//operator perkalian *
+
+let a = x*y
+console.log(a)
+
+//aritmatika
+let b = (100 + 50) * a
+console.log(b)
+
+//oeprator penugasan
+
+let p  = 10
+p += 5
+
+console.log(p)
+
+//operator perbandingan
+
+let text1 = "A"
+let text2 = "B"
+
+let result = text1 < text2
+console.log(result)
+
+let text3 = text1 + " " + text2
+
+console.log(text3)
+
+let newText = "FirstName : "
+ newText = "Aldo Wiranata"
+ console.log(newText)
