584d22c0651e3d475e095e862705ac49e9da4bd2
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-12-25 13:21:53 +0700

    file : add Assignment.js

---
+++ js/Assignment.js
@@ -0,0 +1,48 @@
+let x  = 10
+let y = 2
+ 
+
+// x += y 
+// console.log(x)
+
+// x -= y
+// console.log(x)
+
+// x *= y
+// console.log(x)
+
+// x /= y
+// console.log(x)
+
+
+// console.log(x)
+
+// x **= y
+// console.log(x)
+
+// x <<= y 
+// console.log(x)
+// x >>= y
+// console.log(x)
+
+// x >>>= y
+// console.log(x)
+
+// x &= y
+// console.log(x)
+
+// x ^= y 
+// console.log(x)
+
+// x |= y
+// console.log(x)
+
+x &&= y
+console.log(x)
+
+x ||= y
+console.log(x)
+
+x ??= y
+console.log(x) 
+
