267b9f743bf83ba4aeecf63fad0ba4ce5ef57e65
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-01-03 23:07:19 +0700

    file : add StringSearch.js

---
+++ js/StringSearch.js
@@ -0,0 +1,6 @@
+let text = "aku aldo wiranata 18 'aldo'"
+let index  = text.indexOf("wiranata") 
+let index2 = text.lastIndexOf("18")
+let index3 = text.indexOf("aldo",7)
+let index4 = text.lastIndexOf("aldo",15)
+console.log(index , index2, index3 , index4)
