e4bdcfec9659a56a4626ebd633aedd2d7929411a
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-01-08 12:24:18 +0700

    file : add Array.js

---
+++ js/Array.js
@@ -0,0 +1,13 @@
+const day = ["Sunday" ,"Monday","Thursday"]
+const newDay = [
+    "Wednesday",
+    "Thursday",
+    "Friday"
+]
+const endDay = []
+endDay[0] = "Sunday"
+endDay[1] = "Monday"
+
+const cars = new Array("BMW" , "Toyota")
+console.log(day , newDay , endDay , cars)
+
