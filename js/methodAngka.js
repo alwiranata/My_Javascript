cc3ddacba67b53ef65548c94ac3bff6d4f92f4ca
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-01-08 11:05:44 +0700

    file : add methodAngka.js

---
+++ js/methodAngka.js
@@ -0,0 +1,9 @@
+let x = 1.2323
+x.toString()
+let y = 3.20120912000
+y.toPrecision(2)
+console.log(y) 
+const date = Number(new Date("2025-01-08"))
+console.log(date)
+const float= parseFloat("10.33")
+console.log(typeof float)
