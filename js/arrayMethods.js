3ed39a411a4d28995c7c1c1a797702bb8be4a20e
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-01-11 21:04:18 +0700

    file : update arrayMethods.js

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -33,3 +33,25 @@ console.log(myChildrem)
 const add = myGirls.concat(myBoys,myBaby)
 console.log(add )
 console.log(myGirls.concat("i dont know"))
+const newfruits = ["Banana"," Orange", "apple ", "Manggo"] 
+console.log(newfruits.copyWithin(2,0))
+console.log(newfruits.copyWithin(2,0,2))
+
+const newArr = [[1,2],[3,4],[5,6]]
+console.log(newArr.flat())
+
+const myValue = [1,2,3,4,5,6]
+console.log(myValue.flatMap(x=> [x, x *10]))
+console.log(myValue.splice(2,0, "lemon" ,"kiwi"))
+const pfruits = ["Banana", "Orange", "Apple", "Mango"]
+console.log(pfruits.splice(2, 2, "Kiwi"))
+console.log(pfruits)
+
+const fruitsa = ["Banana", "Orange", "Apple", "Mango"];
+console.log("splice" ,fruitsa.splice(0 , 1))
+console.log(fruitsa)
+
+const fruitsp = ["Banana", "Orange", "Lemon", "Apple", "Mango"]
+const citrus = fruitsp.slice(1,3)
+console.log(citrus)
+console.log(fruitsp) 
